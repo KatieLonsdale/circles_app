946a8cec736597f9112261418999dd117d18b7e9
Katie Lonsdale <katie952@gmail.com>
2024-06-10 20:16:46 -0600

    add Factory Bot fields for User Factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { BCrypt::Password.create('password') }
+    display_name { Faker::Name.name }
+    notification_frequency { %w[live hourly daily].sample }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
