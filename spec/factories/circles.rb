5bf9ddf96c3ed73c47ac1d0b3da394ea2460cd53
Katie Lonsdale <katie952@gmail.com>
2024-06-14 13:33:31 -0600

    update circles factory to not create members

--- spec/factories/circles.rb
+++ spec/factories/circles.rb
@@ -6,16 +6,5 @@ FactoryBot.define do
     updated_at { Time.now }
 
     association :user, factory: :user
-
-    transient do
-      members_count { 3 }  # Default number of members to create
-    end
-
-    after(:build) do |circle, evaluator|
-      members = FactoryBot.create_list(:user, evaluator.members_count)
-      members_array = members.map { |member| { id: member.id } }
-      circle.members = members_array.to_json
-    end
-
   end
 end
