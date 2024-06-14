5e316868afad0ff51ad636aadbb94375b8569e09
Katie Lonsdale <katie952@gmail.com>
2024-06-14 16:32:42 -0600

    Add retrieval methods for getting user's circles

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,14 @@ class User < ApplicationRecord
             format: { with: EMAIL_REGEX, message: "must be a valid email address" }
   validates_presence_of :password
   validates_presence_of :display_name
+
+  def get_all_circles
+    owned_circles = circles
+    circle_members = get_circle_member_circles
+    all_circles = owned_circles + circle_members
+  end
+
+  def get_circle_member_circles
+    Circle.joins(:circle_members).where("circle_members.user_id = ?", self.id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,41 @@ RSpec.describe User, type: :model do
         # fill out
       end
     end
+
+    describe 'get_all_circles' do
+      it 'gets all circles that a user belongs to or owns' do
+        create_list(:circle, 10)
+        # assign a new user to 3 circles
+        user = create(:user)
+        not_owned_circles = Circle.all.sample(3)
+        not_owned_circles.each do |circle|
+          create(:circle_member, user_id: user.id, circle_id: circle.id)
+        end
+        # create circles that user owns
+        owned_circles = create_list(:circle, 2, user_id: user.id)
+
+        all_circles = owned_circles + not_owned_circles
+
+        test_result_circles = user.get_all_circles
+
+        expect(test_result_circles.sort).to eq(all_circles.sort)
+      end
+    end
+
+    describe 'get_member_circle_circles' do
+      it 'gets all circles that a user is a member of' do
+        create_list(:circle, 10)
+        # assign a new user to 3 circles
+        user = create(:user)
+        user_circles = Circle.all.sample(5)
+        user_circles.each do |circle|
+          create(:circle_member, user_id: user.id, circle_id: circle.id)
+        end
+
+        test_result_circles = user.get_circle_member_circles
+        expect(test_result_circles.sort).to eq(user_circles.sort)
+      end
+    end
   end
 end
 
