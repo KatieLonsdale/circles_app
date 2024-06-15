04fac5b8fa32bf1c263a768c103a7a80db42fa64
Katie Lonsdale <katie952@gmail.com>
2024-06-15 16:06:57 -0600

    add dependency into Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :circle
   has_many :comments, dependent: :destroy
   has_many :post_user_reactions, dependent: :destroy
-  has_many :content
+  has_many :content, dependent: :destroy
   has_many :comment_user_reactions, through: :comments
 
   validates_presence_of :circle_id
