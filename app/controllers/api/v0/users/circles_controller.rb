525c03eaf0bfcbd0bb981a81cd4cf4505ec825c6
Katie Lonsdale <katie952@gmail.com>
2024-06-14 20:21:32 -0600

    add create and destroy methods to circles controller

--- app/controllers/api/v0/users/circles_controller.rb
+++ app/controllers/api/v0/users/circles_controller.rb
@@ -1,13 +1,23 @@
 class Api::V0::Users::CirclesController < ApplicationController
   before_action :get_user
+
   def index
     all_circles = @user.get_all_circles
     render json: CircleSerializer.new(all_circles)
   end
 
-  def show
+  def create
+    circle = Circle.create!(circle_params)
+    render json: CircleSerializer.new(circle), status: :created
+  end
+
+  def destroy
     circle = Circle.find(params[:id])
-    render json: CircleSerializer.new(circle)
+    if @user.id != circle.user_id || !@user.authenticate(params[:password])
+      render json: {"errors": "Unauthorized"}, status: :unauthorized
+    else
+      circle.destroy!
+    end
   end
 
   private
@@ -15,7 +25,7 @@ class Api::V0::Users::CirclesController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
-  def user_params
+  def circle_params
     params.permit(:user_id, :name, :description)
   end
 end
