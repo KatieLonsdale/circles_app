f833432c6d9a2bd7c8e78d38c0039e77051b3ee6
Katie Lonsdale <katie952@gmail.com>
2024-08-10 14:24:46 -0600

    Add author_display_name method and test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Comment, type: :model do
     it { should validate_presence_of :author_id }
     it { should validate_presence_of :comment_text }
   end
+
+  describe 'instance methods' do
+    describe 'author_display_name' do
+      it 'should return the display name of the author' do
+        users = create_list(:user, 2)
+        posts = create_list(:post, 2)
+        comment_1 = create(:comment, post_id: posts[0].id, author_id: users[0].id)
+        comment_2 = create(:comment, post_id: posts[1].id, author_id: users[1].id)
+        comment_3 = create(:comment, post_id: posts[0].id, author_id: users[1].id)
+
+        expect(comment_1.author_display_name).to eq(users[0].display_name)
+        expect(comment_2.author_display_name).to eq(users[1].display_name)
+        expect(comment_3.author_display_name).to eq(users[1].display_name)
+      end
+    end
+  end
 end
