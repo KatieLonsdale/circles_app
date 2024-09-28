9870fc111b498427b32814477d5643668ac7f82a
Katie Lonsdale <katie952@gmail.com>
2024-09-27 21:25:49 -0600

    add content serializer

--- app/serializers/content_serializer.rb
+++ app/serializers/content_serializer.rb
@@ -1,4 +1,9 @@
 class ContentSerializer
   include JSONAPI::Serializer
-  attributes :id, :video_url, :image_url
+
+  attributes :id, :video_url
+
+  attribute :presigned_image_url do |object|
+    ImageUploadService.new.create_presigned_url(object.image_url) if object.image_url
+  end
 end
