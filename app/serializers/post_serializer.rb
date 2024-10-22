e67e1e46ef1d0652a325d4a83d9ce46879fe954d
Katie Lonsdale <katie952@gmail.com>
2024-10-21 20:13:55 -0600

    add circle name

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -7,9 +7,13 @@ class PostSerializer
              :updated_at,
              :circle_id
 
-attribute :author_display_name do |object|
-  object.author_display_name
-end
+  attribute :author_display_name do |object|
+    object.author_display_name
+  end
+
+  attribute :circle_name do |object|
+    object.circle.name
+  end
 
   attribute :contents do |post|
     ContentSerializer.new(post.content)
