6cb39b2381fdc385ac7b0818c41186fc15ba0dfd
Katie Lonsdale <katie952@gmail.com>
2024-12-06 10:31:57 -0700

    change create session to return user attributes and update tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       token = JsonWebTokenService.encode(user_id: user.id)
-      render json: { token: token }, status: :ok
+      render json: { 
+        token: token,
+        user: UserSerializer.new(user)
+        }, status: :ok
     else
       render json: { error: 'Invalid credentials' }, status: :unauthorized
     end
