d37e5eb9671901724231cac925e7585fe2971df2
Katie Lonsdale <katie952@gmail.com>
2025-04-30 10:59:20 -0600

    move helper method

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -1,4 +1,15 @@
 # Initialize Firebase Cloud Messaging client
+
+# In production, write the credentials content from env var to a temp file
+require 'tempfile'
+
+def create_temp_credentials_file
+  credentials_content = ENV['GOOGLE_APPLICATION_CREDENTIALS_PATH']
+  credentials_file = Tempfile.new(['firebase_credentials', '.json'])
+  credentials_file.write(credentials_content)
+  credentials_file.path
+end
+
 if Rails.env.production?
   # Initialize FCM client with the temp file path
   FCM_CLIENT = FCM.new(
@@ -11,14 +22,4 @@ else
     Figaro.env.GOOGLE_APPLICATION_CREDENTIALS_PATH,
     Figaro.env.FIREBASE_PROJECT_ID
   )
-end 
-
-  # In production, write the credentials content from env var to a temp file
-
-def create_temp_credentials_file
-  require 'tempfile'
-  credentials_content = ENV['GOOGLE_APPLICATION_CREDENTIALS_PATH']
-  credentials_file = Tempfile.new(['firebase_credentials', '.json'])
-  credentials_file.write(credentials_content)
-  credentials_file.path
 end
