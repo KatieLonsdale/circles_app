cad8abcfb35c458b5b750720f29fb77f6fb4365c
Katie Lonsdale <katie952@gmail.com>
2025-05-20 15:27:06 -0600

    return comments in hierarchy

--- spec/requests/api/v0/comment_requests_spec.rb
+++ spec/requests/api/v0/comment_requests_spec.rb
@@ -18,6 +18,64 @@ RSpec.describe 'Comments API', type: :request do
       expect(data.count).to eq(3)
       returned_comment_ids = data.map { |comment| comment[:id].to_i }
       expect(returned_comment_ids.sort).to eq(comments_to_retrieve.map(&:id).sort)
+      
+      # Check for author_display_name in response
+      expect(data.first[:attributes]).to have_key(:author_display_name)
+    end
+
+    it 'includes all replies to comments in hierarchical order' do
+      User.destroy_all
+      users = create_list(:user, 4)
+      #add users to circle
+      circle = create(:circle, user_id: users[0].id)
+      create(:circle_member, user_id: users[1].id, circle_id: circle.id)
+      create(:circle_member, user_id: users[2].id, circle_id: circle.id)
+      create(:circle_member, user_id: users[3].id, circle_id: circle.id)
+      #post with two top-level comments
+      post = create(:post, author_id: users[0].id, circle_id: circle.id)
+      # comment with no replies
+      create(:comment, post_id: post.id, author_id: users[3].id)
+      # thread: comment, reply, and two sub-replies
+      comment = create(:comment, post_id: post.id, author_id: users[1].id)
+      reply = create(:comment, post_id: post.id, author_id: users[2].id, parent_comment_id: comment.id)
+      sub_reply_1 = create(:comment, post_id: post.id, author_id: users[0].id, parent_comment_id: reply.id)
+      sub_reply_2 = create(:comment, post_id: post.id, author_id: users[2].id, parent_comment_id: reply.id)
+
+      get "/api/v0/users/#{users[0].id}/circles/#{post.circle_id}/posts/#{post.id}/comments"
+      expect(response.status).to eq(200)
+      comments = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(comments.count).to eq(2)
+      
+      # Check for top-level comment attributes
+      comment_response = comments.find { |c| c[:id].to_i == comment.id }
+      expect(comment_response[:attributes][:comment_text]).to eq(comment.comment_text)
+      expect(comment_response[:attributes][:author_id]).to eq(comment.author_id)
+      expect(comment_response[:attributes]).to have_key(:author_display_name)
+      
+      # Check for nested replies
+      replies = comment_response[:attributes][:replies][:data]
+      expect(replies.count).to eq(1)
+      
+      # Check reply attributes
+      reply_response = replies.first
+      expect(reply_response[:attributes][:parent_comment_id]).to eq(comment.id)
+      expect(reply_response[:attributes][:comment_text]).to eq(reply.comment_text)
+      expect(reply_response[:attributes]).to have_key(:author_display_name)
+      
+      # Check sub-replies
+      sub_replies = reply_response[:attributes][:replies][:data]
+      expect(sub_replies.count).to eq(2)
+      
+      # Check sub-reply attributes
+      sub_reply_1_response = sub_replies.find { |sr| sr[:id].to_i == sub_reply_1.id }
+      expect(sub_reply_1_response[:attributes][:parent_comment_id]).to eq(reply.id)
+      expect(sub_reply_1_response[:attributes][:comment_text]).to eq(sub_reply_1.comment_text)
+      expect(sub_reply_1_response[:attributes]).to have_key(:author_display_name)
+      
+      sub_reply_2_response = sub_replies.find { |sr| sr[:id].to_i == sub_reply_2.id }
+      expect(sub_reply_2_response[:attributes][:parent_comment_id]).to eq(reply.id)
+      expect(sub_reply_2_response[:attributes][:comment_text]).to eq(sub_reply_2.comment_text)
+      expect(sub_reply_2_response[:attributes]).to have_key(:author_display_name)
     end
   end
 
@@ -39,12 +97,14 @@ RSpec.describe 'Comments API', type: :request do
       data = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(data[:type]).to eq("comment")
-      attribtes = data[:attributes]
-      expect(attribtes[:id]).to eq(@comment_to_return.id)
-      expect(attribtes[:comment_text]).to eq(@comment_to_return.comment_text)
-      expect(attribtes[:author_id]).to eq(@comment_to_return.author_id)
-      expect(attribtes[:post_id]).to eq(@comment_to_return.post_id)
-      expect(attribtes[:parent_comment_id]).to eq(@comment_to_return.parent_comment_id)
+      attributes = data[:attributes]
+      expect(attributes[:id]).to eq(@comment_to_return.id)
+      expect(attributes[:comment_text]).to eq(@comment_to_return.comment_text)
+      expect(attributes[:author_id]).to eq(@comment_to_return.author_id)
+      expect(attributes[:post_id]).to eq(@comment_to_return.post_id)
+      expect(attributes[:parent_comment_id]).to eq(@comment_to_return.parent_comment_id)
+      expect(attributes).to have_key(:author_display_name)
+      expect(attributes).to have_key(:replies)
     end
 
     it 'returns 404 Not Found if the comment does not exist' do
@@ -76,6 +136,9 @@ RSpec.describe 'Comments API', type: :request do
       expect(data[:attributes][:author_id]).to eq(member.id)
       expect(data[:attributes][:post_id]).to eq(post.id)
       expect(data[:attributes][:parent_comment_id]).to eq(nil)
+      expect(data[:attributes]).to have_key(:author_display_name)
+      expect(data[:attributes]).to have_key(:replies)
+      expect(data[:attributes][:replies][:data]).to be_empty
 
       expect(Comment.count).to eq(1)
 
@@ -91,6 +154,9 @@ RSpec.describe 'Comments API', type: :request do
 
       expect(data[:attributes][:comment_text]).to eq("this is a sub-comment")
       expect(data[:attributes][:parent_comment_id]).to eq(comment.id)
+      expect(data[:attributes]).to have_key(:author_display_name)
+      expect(data[:attributes]).to have_key(:replies)
+      expect(data[:attributes][:replies][:data]).to be_empty
 
       expect(Comment.count).to eq(2)
     end
@@ -118,6 +184,8 @@ RSpec.describe 'Comments API', type: :request do
 
       expect(data[:attributes][:comment_text]).to eq("This should be allowed")
       expect(data[:attributes][:updated_at]).to be > time.iso8601(3)
+      expect(data[:attributes]).to have_key(:author_display_name)
+      expect(data[:attributes]).to have_key(:replies)
       time = data[:attributes][:updated_at]
 
       params = {comment_text: 'This should not be allowed'}
