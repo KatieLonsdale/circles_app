bdad371a63153e59261b45d2d025056e39611c19
Katie Lonsdale <katie952@gmail.com>
2025-05-30 14:11:35 -0600

    update log

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -20,7 +20,7 @@ class NotificationSerializer
     if notification.notifiable_type.downcase == 'post'
       notification.notifiable_id
     elsif notification.notifiable_type.downcase == 'comment'
-      print(notification)
+      print("comment id: #{notification.notifiable_id}")
       notification.notifiable.post_id
     end
   end
