8ee20a463852834b48a892559d9926023b6d03c0
katielonsdale <katie952@gmail.com>
2025-06-14 14:07:08 -0600

    send user notifications token to app

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :display_name, :notification_frequency, :last_tou_acceptance
+  attributes :id, :email, :display_name, :notification_frequency, :last_tou_acceptance, :notifications_token
   
   attribute :friends_count do |user|
     user.all_friends.count
